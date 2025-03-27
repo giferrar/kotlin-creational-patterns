8a67f3fa2b8ecf82b4bbe4cb13f575ab755267e5
Ferrario.Gianluca <Ferrario.Gianluca@Bank-Verlag.de>
2025-03-27 14:35:57 +0100

    Exercise 1. Person class implemented and inaccuracy of the log message is identified

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,6 +10,23 @@ import org.hamcrest.MatcherAssert.assertThat
 
 
 fun main() {
-    println("Hello World!")
+    // Solution to Exercise 1
+    Person("John", "Doe") // Please note how optional parameters help reduce method overloading
+    Person("Bob", "Doe", 25)
+    // Problem: according to the printed message, Moe is not employed, because the init block is part of the primary constructor and is executed before the secondary constructor
+    Person("Moe", "Doe", 40, true)
+}
+
+class Person(name: String, surname: String, age: Int = 18) {
+
+    private var employed: Boolean = false
+
+    constructor(name: String, surname: String, age: Int, employed: Boolean) : this(name, surname, age) {
+        this.employed = employed
+    }
+
+    init {
+        println("Person created with name $name $surname and age $age. $name is ${if (employed) "" else "not "}employed")
+    }
 }
 
