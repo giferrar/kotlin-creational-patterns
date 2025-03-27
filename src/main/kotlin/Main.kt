2e88bad36bdce6e2e4511283cdf4835042c65a5d
Ferrario.Gianluca <Ferrario.Gianluca@Bank-Verlag.de>
2025-03-27 14:41:55 +0100

    Exercise 1. Secondary constructor implemented. Log message from constructor displays the right information

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -15,14 +15,21 @@ fun main() {
     Person("Bob", "Doe", 25)
     // Problem: according to the printed message, Moe is not employed, because the init block is part of the primary constructor and is executed before the secondary constructor
     Person("Moe", "Doe", 40, true)
+    Person("Klaus", "Doe", 70, employed = false, retired = true)
 }
 
 class Person(name: String, surname: String, age: Int = 18) {
 
     private var employed: Boolean = false
+    private var retired: Boolean = false
 
     constructor(name: String, surname: String, age: Int, employed: Boolean) : this(name, surname, age) {
         this.employed = employed
+    }
+
+    constructor(name: String, surname: String, age: Int, employed: Boolean, retired: Boolean) : this(name, surname, age, employed) {
+        this.retired = retired
+        println("Person created with name $name $surname and age $age. $name is ${if (employed) "" else "not "}employed and ${if (retired) "" else "not "}retired")
     }
 
     init {
