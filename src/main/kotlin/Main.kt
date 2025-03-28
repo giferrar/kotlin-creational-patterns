44399c15f1c1d7133e007ce844df9a8912dbaa12
Ferrario.Gianluca <Ferrario.Gianluca@Bank-Verlag.de>
2025-03-28 10:51:28 +0100

    Exercise 6. Refactoring, merge functions step 3. Call generic function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,8 +7,6 @@ import org.example.exercises.factory.abstract.GameFactory
 import org.example.exercises.factory.method.FactoryMethodExercise
 import org.example.exercises.prototype.PrototypeExercise
 import org.example.exercises.singleton.SingletonExercise
-import org.hamcrest.CoreMatchers.`is`
-import org.hamcrest.MatcherAssert.assertThat
 
 
 fun main() {
--- src/main/kotlin/exercises/prototype/PrototypeExercise.kt
+++ src/main/kotlin/exercises/prototype/PrototypeExercise.kt
@@ -4,25 +4,18 @@ class PrototypeExercise {
 
     fun createClones() {
         val circle = Circle(1.0)
-        val clonedCircle = cloneCircle(circle)
+        val clonedCircle = cloneShape(circle)
         println("The area of the cloned circle is ${clonedCircle.area()}")
 
         val square = Square(1.0)
-        val clonedSquare = cloneSquare(square)
+        val clonedSquare = cloneShape(square)
         println("The area of the cloned square is ${clonedSquare.area()}")
-    }
-
-    private fun cloneCircle(circle: Shape): Shape {
-        return cloneShape(circle)
-    }
-
-    private fun cloneSquare(square: Shape): Shape {
-        return cloneShape(square)
     }
 
     // Merge the two functions in 3 steps.
     // Step 1: extract the common code in a generic function
     // Step 2: modify the signature and return of the two functions to match the generic function
+    // Step 3: call the generic function
     private fun cloneShape(shape: Shape): Shape {
         // Here we do some important operations
         return shape.clone()
